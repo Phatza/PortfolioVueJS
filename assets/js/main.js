7a957d4a5d8868e96e1ef64456308b24db989a16
INTEGRATEUR APPLICATION <integrateurappli@Mac-mini-de-INFA203.local>
2016-12-06 17:02:50 +0100

    Intégration Menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,60 @@
-// Instance VueJS
+// My test component
+Vue.component('block', {
+  template:
+    `
+      <div>
+        <p><slot></slot></p>
+      </div>
+    `
+})
 
+Vue.component('block1', {
+  template:
+    `
+      <div>
+        <p><slot></slot></p>
+      </div>
+    `
+})
+
+Vue.component('block2', {
+  template:
+    `
+      <div>
+        <p><slot></slot></p>
+      </div>
+    `
+})
+
+Vue.component('responsive-menu', {
+  template:
+    `
+    <div class="responsiveMenu">
+      <ul class="menu-list">
+        <li><a href="#">Dashboard</a></li>
+        <li><a href="#">Customers</a></li>
+      </ul>
+    </div>
+    `
+})
+
+
+
+
+// Instance VueJS
 let app = new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    message: 'Hello World',
+    visible: true,
+    mobileMenuVisible: false
+  },
+  methods: {
+    isActive() {
+      this.visible = !this.visible
+    },
+    isToggle() {
+      this.mobileMenuVisible = !this.mobileMenuVisible
+    }
+  }
 })
