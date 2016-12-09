ab6e53ac52ca5c392f1a03d51964945f6fd1cde2
Phatza <b.haphuoc@gmail.com>
2016-12-09 01:41:05 +0100

    add active class on click visibleLink

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,17 +5,23 @@ Vue.component('navigation', {
     `
     <ul class="menu-list">
       <li class="has-text-centered" v-for="link in links">
-        <a @click="currentLink(link)" href="javascript:;">{{ link }}</a>
+        <a @click="currentLink(link)" href="javascript:;" :class="{'is-active' : visibleLink === link}" :data-hover="link">{{ link }}</a>
       </li>
     </ul>
     `,
   methods: {
-    currentLink(link) {
-      this.$parent.visibleLink = link
+    currentLink(elem) {
+      this.$parent.visibleLink = elem
+    }
+  },
+  computed: {
+    visibleLink() {
+      return this.$parent.visibleLink
     }
   }
 })
 
+// Project Section Component
 Vue.component('project', {
   template:
     `
@@ -45,6 +51,74 @@ Vue.component('project', {
   }
 })
 
+// WP Project Component
+Vue.component('wordpress', {
+  template:
+    `
+    <div class="columns">
+      <div class="column is-one-third" v-for='site in wpSites'>
+        <<div class="card">
+          <div class="card-image">
+            <figure class="image is-3by2">
+              <img :src="site.image" alt="">
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="content">
+              <h2>{{ site.title }}</h2>
+              <a :href="site.url" class="button">
+                <span class="icon">
+                <svg height="25px" width="25px" viewBox="0 0 30 30">
+                  <path fill="#666" d="M17.9,17.39C17.64,16.59 16.89,16 16,16H15V13A1,1 0 0,0 14,12H8V10H10A1,1 0 0,0 11,9V7H13A2,2 0 0,0 15,5V4.59C17.93,5.77 20,8.64 20,12C20,14.08 19.2,15.97 17.9,17.39M11,19.93C7.05,19.44 4,16.08 4,12C4,11.38 4.08,10.78 4.21,10.21L9,15V16A2,2 0 0,0 11,18M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
+                </svg>
+                </span>
+                <span>Accèdez au site</span>
+              </a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    `,
+    data() {
+      return {
+        wpSites: {
+          bettina: {
+            title: 'Bettina',
+            image: 'http://placehold.it/300x225',
+            url: 'http://fldslld'
+          },
+          bastide: {
+            title: 'Bastide',
+            image: 'http://placehold.it/300x225',
+            url: 'http://fldslld'
+          },
+          purjus: {
+            title: 'Purjus',
+            image: 'http://placehold.it/300x225',
+            url: 'http://fldslld'
+          },
+          bateauGitePorquerolles: {
+            title: 'Fa\'auva',
+            image: 'http://placehold.it/300x225',
+            url: 'http://fldslld'
+          },
+          couleurSoleil: {
+            title: 'Couleur Soleil',
+            image: 'http://placehold.it/300x225',
+            url: 'http://fldslld'
+          },
+          Cellois: {
+            title: 'Le Cellois',
+            image: 'http://placehold.it/300x225',
+            url: 'http://fldslld'
+          }
+        }
+      }
+    }
+})
+
 // Instance VueJS
 let app = new Vue({
   el: '#app',
@@ -54,9 +128,6 @@ let app = new Vue({
     mobileMenuVisible: false
   },
   methods: {
-    isActive() {
-      this.displayNone = !this.displayNone
-    },
     // Toggle show/hide Menu Mobile & Tablet Devices
     isToggle() {
       this.mobileMenuVisible = !this.mobileMenuVisible
